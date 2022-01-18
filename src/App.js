90bd03777cf4731ae8e6c18a33488516c6bd39ee
sabarht <sabarht@gmail.com>
2022-01-18 13:08:56 +0100

    Added github

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Saba Rahrotaban and is{" "}
+        <a href="https://nervous-haibt-37ab66.netlify.app/" target="_blank">
+          open-sourced on Github.
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
